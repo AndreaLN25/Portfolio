6627c7e51e2136773d416c0421ade2771729ccd6
AndreaLN25 <andrealn14@gmail.com>
2024-09-25 17:39:30 +0200

    Form validations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,24 +8,62 @@ const Contact = () => {
         <div className='form-grid'>
           <div className='form-group'>
             <label className='form-label'>Name</label>
-            <input className='form-input' type='text' name='name' />
+            <input
+              className='form-input'
+              type='text'
+              name='name'
+              required
+              minLength='2'
+              maxLength='50'
+              placeholder='Enter your name'
+            />
           </div>
           <div className='form-group'>
             <label className='form-label'>Phone</label>
-            <input className='form-input' type='number' name='phone' />
+            <input
+              className='form-input'
+              type='tel'
+              name='phone'
+              required
+              minLength='10'  // Mínimo 10 dígitos
+              maxLength='15'  // Máximo 15 dígitos
+              pattern='[0-9]{10,15}' // Solo dígitos, entre 10 y 15
+              placeholder='Enter your phone number'
+            />
           </div>
         </div>
         <div className='form-group'>
           <label className='form-label'>Email</label>
-          <input className='form-input' type='email' name='email' />
+          <input
+            className='form-input'
+            type='email'
+            name='email'
+            required
+            placeholder='Enter your email'
+          />
         </div>
         <div className='form-group'>
           <label className='form-label'>Subject</label>
-          <input className='form-input' type='text' name='subject' />
+          <input
+            className='form-input'
+            type='text'
+            name='subject'
+            required
+            minLength='2'
+            maxLength='100'
+            placeholder='Enter subject'
+          />
         </div>
         <div className='form-group'>
           <label className='form-label'>Message</label>
-          <textarea className='form-textarea' rows='4.5' name='message' />
+          <textarea
+            className='form-textarea'
+            rows='4.5'
+            name='message'
+            required
+            minLength='10'
+            placeholder='Write your message here...'
+          />
         </div>
         <button className='form-button'>
           Send Message
